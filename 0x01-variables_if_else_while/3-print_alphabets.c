4a6b0126d40574cbfbbedae54186e611fdd4b3ff
Muhammad Modu Kunjo <mmkunjo@gmail.com>
2022-09-09 15:26:09 -0700

    alphabet printing

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - program startimg point
+ * Description: Program for printing lower and uppercase alphabet
+ * Return: Alaways 0 (Success)
+ */
+int main(void)
+{
+	ch n;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}

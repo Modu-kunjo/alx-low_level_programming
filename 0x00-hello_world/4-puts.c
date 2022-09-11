5efbf9e0d2f49d6d52355804842c3fdf0b0dc485
Muhammad Modu Kunjo <mmkunjo@gmail.com>
2022-09-11 03:16:23 -0700

    alx introduction to c programming language

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Description: Its a program thats prints Programming is like building a
+ * mulitilingual puzzle
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printint strings
+ * Description: using printf
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}

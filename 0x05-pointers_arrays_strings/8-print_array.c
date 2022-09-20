be0d1d637a69e46415db82bc0d7861cf99a1bd5f
Muhammad Modu Kunjo <mmkunjo@gmail.com>
2022-09-20 13:21:31 -0700

    printing array elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,226 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print 'n' element of an array
+ * @a: int type array pointer
+ * @n: int type integer
+ * Description: Numbers must be seperated by comma 
+ * only used _putchar to print
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; i++)
+	{
+		printf("%d"m a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print 'n' element of an array
+ * @a: int type array pointer
+ * @n: int type integer
+ * Description: Numbers must be seperated by comma 
+ * only used _putchar to print
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; i++)
+	{
+		printf("%d"m a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print 'n' element of an array
+ * @a: int type array pointer
+ * @n: int type integer
+ * Description: Numbers must be seperated by comma 
+ * only used _putchar to print
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; i++)
+	{
+		printf("%d"m a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print 'n' element of an array
+ * @a: int type array pointer
+ * @n: int type integer
+ * Description: Numbers must be seperated by comma 
+ * only used _putchar to print
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; i++)
+	{
+		printf("%d"m a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print 'n' element of an array
+ * @a: int type array pointer
+ * @n: int type integer
+ * Description: Numbers must be seperated by comma 
+ * only used _putchar to print
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; i++)
+	{
+		printf("%d"m a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print 'n' element of an array
+ * @a: int type array pointer
+ * @n: int type integer
+ * Description: Numbers must be seperated by comma 
+ * only used _putchar to print
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; i++)
+	{
+		printf("%d"m a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print 'n' element of an array
+ * @a: int type array pointer
+ * @n: int type integer
+ * Description: Numbers must be seperated by comma 
+ * only used _putchar to print
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; i++)
+	{
+		printf("%d"m a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print 'n' element of an array
+ * @a: int type array pointer
+ * @n: int type integer
+ * Description: Numbers must be seperated by comma 
+ * only used _putchar to print
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; i++)
+	{
+		printf("%d"m a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print 'n' element of an array
+ * @a: int type array pointer
+ * @n: int type integer
+ * Description: Numbers must be seperated by comma 
+ * only used _putchar to print
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; i++)
+	{
+		printf("%d"m a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}

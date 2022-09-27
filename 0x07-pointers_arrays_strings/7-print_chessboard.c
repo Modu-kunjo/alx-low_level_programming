bfb850b32ea25783766d3727192897286e4a7df8
Muhammad Modu Kunjo <mmkunjo@gmail.com>
2022-09-26 17:10:52 -0700

    fixed a bug in print_chessboard function

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,18 +10,12 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	i = 0;
-	j = 0;
-	while (i < 64)
+	for (i = 0; i < 8; i++)
 	{
-		if (i % 8 == 0 && i != 0)
+		for (j = 0; i < 8; j++)
 		{
-			j = i;
-			_putchar('\n');
+			printf("%c", a[i][j]);
 		}
-
-		_putchar(a[i / 8][i - j]);
-		i++;
+		printf("\n");
 	}
-	_putchar('\n');
 }

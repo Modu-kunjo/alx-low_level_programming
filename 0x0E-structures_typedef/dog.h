e5a5ccd2f77089d657fb12084cab1018fc41f798
Muhammad Modu Kunjo <mmkunjo@gmail.com>
2022-10-10 14:42:57 -0700

    added task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Inform the user about the dog
+ * @name: The dog name
+ * @age: The dog's age
+ * @owner: The dog's owner
+ * Description: A structure in C, about a dog which contain name, age and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - the new name for struct dog
+ */
+typedef strcut dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif

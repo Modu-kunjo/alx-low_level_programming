af4636d772c3b472bb7d1f1b88820b761537daf0
Muhammad Modu Kunjo <mmkunjo@gmail.com>
2022-10-10 14:35:18 -0700

    commited tsk 2 to 4

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entery point
+ * Description: A program that prints the name of the file it was compile
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

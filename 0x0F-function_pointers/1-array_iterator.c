9c65b3d95a2ac4f5de9ddac8afb04d4d1eb76b55
Muhammad Modu Kunjo <mmkunjo@gmail.com>
2022-10-12 16:53:56 -0700

    commit task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,13 +11,13 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	unsigned int i;
 
 	if (array && action)
 	{
 		for (i = 0; i < size; i++)
 		{
-			actionn(array[i]);
+			action(array[i]);
 		}
 	}
 }
